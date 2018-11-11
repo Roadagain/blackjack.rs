6970ef2658e4aee7ba3d8542a2312c7fca79e470
Roadagain <ringoh72@gmail.com>
2018-11-11 21:00:18 +0900

    feat(score): スコア実装

---
+++ src/card/score.rs
@@ -0,0 +1,36 @@
+use super::Rank;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Score {
+    score: u32,
+}
+
+impl Score {
+    pub fn new(rank: Rank) -> Self {
+        let score = match rank {
+            Rank::JACK => 10,
+            Rank::QUEEN => 10,
+            Rank::KING => 10,
+            n => n as u32,
+        };
+        Self { score }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Rank;
+    use super::Score;
+
+    #[test]
+    fn jqk_is_ten() {
+        let jack_score = Score::new(Rank::JACK);
+        assert_eq!(jack_score.score, 10);
+
+        let queen_score = Score::new(Rank::QUEEN);
+        assert_eq!(queen_score.score, 10);
+
+        let king_score = Score::new(Rank::KING);
+        assert_eq!(king_score.score, 10);
+    }
+}
