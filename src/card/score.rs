0e4ce40c78d64036a5180a67e215e2c2fc99fd4a
Roadagain <ringoh72@gmail.com>
2018-11-12 22:57:49 +0900

    feat(score): Aは11点

--- src/card/score.rs
+++ src/card/score.rs
@@ -8,6 +8,7 @@ pub struct Score {
 impl Score {
     pub fn new(rank: Rank) -> Self {
         let score = match rank {
+            Rank::ACE => 11,
             Rank::JACK => 10,
             Rank::QUEEN => 10,
             Rank::KING => 10,
@@ -32,5 +33,11 @@ mod test {
 
         let king_score = Score::new(Rank::KING);
         assert_eq!(king_score.score, 10);
+    }
+
+    #[test]
+    fn ace_is_eleven() {
+        let ace_score = Score::new(Rank::ACE);
+        assert_eq!(ace_score.score, 11);
     }
 }
