2735993fedfb37b8104ab329811e7616e6719799
Roadagain <ringoh72@gmail.com>
2018-11-12 23:25:52 +0900

    refactor(score): singleとdoubleの直和型にする

--- src/card/score.rs
+++ src/card/score.rs
@@ -2,24 +2,24 @@ use super::Rank;
 use std::ops::Add;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Score {
-    min: u32,
-    max: u32,
+pub enum Score {
+    Single { value: u32 },
+    Double { min: u32, max: u32 },
 }
 
 impl Score {
     pub fn new(rank: Rank) -> Self {
-        let min = match rank {
-            Rank::JACK => 10,
-            Rank::QUEEN => 10,
-            Rank::KING => 10,
-            n => n as u32,
-        };
-        let max = match rank {
-            Rank::ACE => 11,
-            _ => min,
-        };
-        Self { min, max }
+        if rank == Rank::ACE {
+            Score::Double { min: 1, max: 11 }
+        } else {
+            let value = match rank {
+                Rank::JACK => 10,
+                Rank::QUEEN => 10,
+                Rank::KING => 10,
+                n => n as u32,
+            };
+            Score::Single { value }
+        }
     }
 }
 
@@ -28,9 +28,12 @@ impl Add<Rank> for Score {
 
     fn add(self, rank: Rank) -> Self {
         let inc = rank as u32;
-        Score {
-            min: self.min + inc,
-            max: self.max + inc,
+        match self {
+            Score::Single { value } => Score::Single { value: value + inc },
+            Score::Double { min, max } => Score::Double {
+                min: min + inc,
+                max: max + inc,
+            },
         }
     }
 }
@@ -43,30 +46,43 @@ mod test {
     #[test]
     fn jqk_is_ten() {
         let jack_score = Score::new(Rank::JACK);
-        assert_eq!(jack_score.min, 10);
-        assert_eq!(jack_score.max, 10);
+        match jack_score {
+            Score::Single { value } => assert_eq!(value, 10),
+            _ => assert!(false),
+        }
 
         let queen_score = Score::new(Rank::QUEEN);
-        assert_eq!(queen_score.min, 10);
-        assert_eq!(queen_score.max, 10);
+        match queen_score {
+            Score::Single { value } => assert_eq!(value, 10),
+            _ => assert!(false),
+        }
 
         let king_score = Score::new(Rank::KING);
-        assert_eq!(king_score.min, 10);
-        assert_eq!(king_score.max, 10);
+        match king_score {
+            Score::Single { value } => assert_eq!(value, 10),
+            _ => assert!(false),
+        }
     }
 
     #[test]
     fn ace_is_one_or_eleven() {
         let ace_score = Score::new(Rank::ACE);
-        assert_eq!(ace_score.min, 1);
-        assert_eq!(ace_score.max, 11);
+        match ace_score {
+            Score::Double { min, max } => {
+                assert_eq!(min, 1);
+                assert_eq!(max, 11)
+            }
+            _ => assert!(false),
+        }
     }
 
     #[test]
     fn add_rank_to_score() {
         let score = Score::new(Rank::SIX);
         let result = score + Rank::SEVEN;
-        assert_eq!(result.min, 13);
-        assert_eq!(result.max, 13);
+        match result {
+            Score::Single { value } => assert_eq!(value, 13),
+            _ => assert!(false),
+        }
     }
 }
